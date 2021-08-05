c6d455425189b38b01af5993eb3eee3231278548
Ritesh Singh <1124ritesh@gmail.com>
2021-08-05 21:13:01 +0530

    fixed form field issue while adding new record
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

--- src/Commands/LiveCrudView.php
+++ src/Commands/LiveCrudView.php
@@ -98,7 +98,7 @@ class LiveCrudView extends GeneratorCommand
         $columns = $model->getFillable();
         $str = '';
         $c = 1;
-        $str .= '@foreach($rows as $row)' . PHP_EOL;
+        $str .= '@forelse($rows as $row)' . PHP_EOL;
         $str .= '<tr>';
         if (config('livecrud.template') == 'tailwind') {
             foreach ($columns as $column) {
@@ -115,7 +115,7 @@ class LiveCrudView extends GeneratorCommand
                                 <a href="#" class="text-indigo-600 hover:text-indigo-900" wire:click="edit({{ $row->id }})">{{ __("Edit") }}</a>
                                 <a href="#" class="text-indigo-600 hover:text-indigo-900" wire:click="confirmDelete({{ $row->id }})">{{ __("Delete") }}</a>
                             </td></tr>';
-            $str .= '@endforeach' . PHP_EOL;
+            $str .= '@empty  <tr><td>No Records Found</td></tr>   @endforelse' . PHP_EOL;
 
         }
         if (config('livecrud.template') == 'bootstrap') {
@@ -140,7 +140,7 @@ class LiveCrudView extends GeneratorCommand
                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                                     </svg></a>
                             </td></tr>';
-            $str .= '@endforeach' . PHP_EOL;
+            $str .= '@empty  <tr><td>No Records Found</td></tr>   @endforelse' . PHP_EOL;
 
         }
         return $str;
